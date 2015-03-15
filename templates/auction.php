0cc17cd66a294b8e5a9139228e130c5d1cea21f3
Diogo Gomes <up201106586@fe.up.pt>
2015-03-15 19:10:55 +0000

    added auction page

---
+++ templates/auction.php
@@ -0,0 +1,171 @@
+<?php
+include "parts/header.php";
+?>
+
+    <div class="container-fluid page-wrapper">
+        <div class="col-xs-12">
+            <div class="item-tile base-container upper col-xs-12">
+                <div style="padding:3px;">
+                    <div class="auction-title col-xs-12">
+                        <h1>Casa com piscina</h1>
+                    </div>
+                    <div class="auction-image col-xs-4">
+                        <img src="img/item-prev.jpg" width="341" height="256" style="border-top-left-radius: 3px;border-top-right-radius: 3px;padding: 10px; float: left;"><br>
+                    </div>
+                    <div class="col-xs-8">
+                        <div class="auction-time-left col-xs-12">
+                            <div class="col-xs-12">
+                                <h3> Tempo restante: 10 minutos</h3>
+                            </div>
+                        </div>
+                        <div class="auction-buying-options col-xs-12">
+                            <div class="auction-bid-action col-xs-6" style="padding-right: 20px;">
+                                <div class="col-xs-3" style="padding-left: 0;">
+                                    <h4> Licitar </h4>
+                                </div>
+                                <div class="input-group" >
+<!--                                    <span class="input-group-addon" id="basic-addon1">Licitar</span>-->
+                                    <input type="text" class="form-control" placeholder="Quantia" aria-describedby="basic-addon1">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-default" type="button">Submeter</button>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="aution-buy-now-button col-xs-7" style="padding-left: 93px; padding-right: 0">
+                                <button type="button" class="btn btn-primary col-xs-9" aria-label="Left Align">
+                                    Comprar Já!
+                                </button>
+                            </div>
+                        </div>
+                        <div class="auction-bid col-xs-12">
+                            <h4 class="col-xs-6">Valor da licitação mais alta:</h4>
+                            <h4 class="col-xs-6">392,23€</h4>
+                        </div>
+                        <div class="auction-buy-now col-xs-12">
+                            <h4 class="col-xs-6">Valor Comprar já:</h4>
+                            <h4 class="col-xs-6">5.000,00€</h4>
+                        </div>
+                        <div class="auction-shipping col-xs-12">
+                            <h4 class="col-xs-6">Portes:</h4>
+                            <h4 class="col-xs-6"> 5,00€</h4>
+                        </div>
+                        <div class="auction-seller col-xs-12">
+                            <h4 class="col-xs-6">Vendido por:</h4>
+                            <h4 class="col-xs-6"> <a href="">Francisco Abreu</a></h4>
+                        </div>
+                        <div class="auction-rating col-xs-12">
+                            <h4 class="col-xs-6">Rating:</h4>
+                            <h4 class="col-xs-6">
+                                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="auction-image col-xs-4" style="padding-bottom: 34px; padding-left: 22px">
+                        <div class="btn-group" role="group" aria-label="...">
+                            <button type="button" class="btn btn-sm">Seguir leilão</button>
+                            <button type="button" class="btn btn-sm">Contactar o vendedor</button>
+                            <button type="button" class="btn btn-sm">Reportar leilão</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12">
+            <div class="item-tile base-container upper col-xs-12">
+                <div style="padding:3px;">
+                    <div class="auction-title col-xs-12">
+                        <h1>Descrição especifica ao item</h1>
+                    </div>
+
+                    <div class="col-xs-6">
+                        <div class="auction-bid col-xs-12">
+                            <h4 class="col-xs-5" style="margin: 0;">Cidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Porto</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Localidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Paranhos</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Numero de Quartos:</h4> <h4 class="col-xs-5" style="margin: 0;"> 5</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Numero de WC:</h4> <h4 class="col-xs-5" style="margin: 0;"> 3</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Area:</h4> <h4 class="col-xs-5" style="margin: 0;"> 60 metros quadrados</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Cidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Porto</h4>
+                        </div>
+                    </div>
+                    <div class="col-xs-6" style="margin-bottom: 34px">
+                        <div class="auction-bid col-xs-12">
+                            <h4 class="col-xs-5" style="margin: 0;">Cidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Porto</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Localidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Paranhos</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Numero de Quartos:</h4> <h4 class="col-xs-5" style="margin: 0;"> 5</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Numero de WC:</h4> <h4 class="col-xs-5" style="margin: 0;"> 3</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Area:</h4> <h4 class="col-xs-5" style="margin: 0;"> 60 metros quadrados</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Cidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Porto</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12">
+            <div class="item-tile base-container upper col-xs-12">
+                <div style="padding:3px;">
+                    <div class="auction-title col-xs-12">
+                        <h1>Descrição do autor</h1>
+                    </div>
+                    <div class="auction-author-description col-xs-12" style="margin-bottom: 34px;">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nunc vel mi congue, egestas lectus vitae, feugiat nulla. Cras lacinia turpis
+                        ut dolor eleifend maximus non eu libero. Integer mattis gravida nisl, nec dictum arcu
+                        scelerisque eu. Integer aliquam justo vel est semper, at dapibus leo
+                        pellentesque. Nam id tristique dolor, quis dignissim libero. Vivamus
+                        ante magna, tempus a arcu pharetra, mattis feugiat nunc. Sed non tellus
+                        in neque auctor consequat. Fusce dignissim velit nibh, id sollicitudin sapien
+                        consequat non. Duis interdum, sem euismod sagittis euismod, libero nisi auctor
+                        purus, in lobortis magna nisi non nibh. Morbi eget augue nec ipsum tempus condimentum.
+                        Integer accumsan tellus ut rhoncus placerat. Cras eget faucibus arcu. Nullam ultrices,
+                        odio sit amet imperdiet pharetra, risus sapien euismod quam, quis euismod nulla nulla a
+                        urna. Nullam id nibh ac urna venenatis blandit sit amet sit amet nibh. Praesent in pretium enim.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12">
+            <div class="item-tile base-container upper col-xs-12">
+                <div style="padding:3px;">
+                    <div class="auction-title col-xs-12">
+                        <h1>Mais detalhes</h1>
+                    </div>
+
+                    <div class="col-xs-6">
+                        <div class="auction-title col-xs-12">
+                            <h2>Metodos de pagamento</h2>
+                        </div>
+                        <div class="auction-bid col-xs-12">
+                            <h4 class="col-xs-5" style="margin: 0;">Cidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Porto</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Localidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Paranhos</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Numero de Quartos:</h4> <h4 class="col-xs-5" style="margin: 0;"> 5</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Numero de WC:</h4> <h4 class="col-xs-5" style="margin: 0;"> 3</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Area:</h4> <h4 class="col-xs-5" style="margin: 0;"> 60 metros quadrados</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Cidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Porto</h4>
+                        </div>
+                    </div>
+                    <div class="col-xs-6" style="margin-bottom: 34px">
+                        <div class="auction-title col-xs-12">
+                            <h2>Envio da encomenda</h2>
+                        </div>
+                        <div class="auction-bid col-xs-12">
+                            <h4 class="col-xs-5" style="margin: 0;">Cidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Porto</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Localidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Paranhos</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Numero de Quartos:</h4> <h4 class="col-xs-5" style="margin: 0;"> 5</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Numero de WC:</h4> <h4 class="col-xs-5" style="margin: 0;"> 3</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Area:</h4> <h4 class="col-xs-5" style="margin: 0;"> 60 metros quadrados</h4>
+                            <h4 class="col-xs-5" style="margin: 0;">Cidade:</h4> <h4 class="col-xs-5" style="margin: 0;"> Porto</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+include "parts/footer.php";
