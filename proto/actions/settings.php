e91f1e8ff600f9e110b8e68afa9ac51136db72eb
Duarte_Morais <duartebrrsmorais@gmail.com>
2015-06-06 22:24:12 +0100

    erro no sql

--- proto/actions/settings.php
+++ proto/actions/settings.php
@@ -6,7 +6,6 @@ include_once($BASE_DIR .'database/users.php');
 
 
 $userId= isset($_GET['user']) ? $_GET["user"] :getUserId() ; // vai buscar o user
-
 //vai buscar o user
 if(!($user=getUser($userId))){
     die("1 nao existe user");// TODO filizar se nao existe
@@ -14,29 +13,20 @@ if(!($user=getUser($userId))){
 //verifica se a password esta correcta
 if(sha1($_POST['password'])!=$user['password']) {
     $_SESSION['error_messages'][] = 'Password errada';
-    header("Location: {$BASE_URL}pages/login.php");
+    header("Location: {$BASE_URL}pages/profile.php");
     exit;
 }
 //verifica se os campos estão todos preenchidos
 
 
-
-$name = strip_tags($_POST['namenew']);
-$password = $_POST['passwordnew'];
-$email = $_POST['emailnew'];
+//$photo = $_FILES['photonew'];
+//$extension = end(explode(".", $photo["namenew"]));
 
 
-$photo = $_FILES['photonew'];
-$extension = end(explode(".", $photo["namenew"]));
-
-
-try {
-    $info=setSettings($userId,$name,$password,$email,$activationCode);
-    move_uploaded_file($photo["tmp_name"], $BASE_DIR . "/images/users/" . $userId . '.' . $extension); // this is dangerous
-    chmod($BASE_DIR . "/images/users/" . $userId . '.' . $extension, 0644);
-} catch (PDOException $e) {
-
-    exit;
+if(setSettings($userId,$_POST['namenew'],$_POST['passwordnew'],$_POST['emailnew'])){
+    $_SESSION['success_messages'][] =  'definições alteradas com sucesso.';
+    header("Location: $BASE_URL" . 'pages/profile.php');
+}else{
+    $_SESSION['error_messages'][] = 'erro na alteração de definições';
+    header("Location: $BASE_URL" . 'pages/settings.php');
 }
-$_SESSION['success_messages'][] = 'Alterações com sucesso';
-header("Location: {$BASE_URL}pages/index.php");
